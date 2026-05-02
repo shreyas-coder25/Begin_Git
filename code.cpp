6052649a09038f65936d8023701314c15f7850a5
shreyas-coder25 <shreyasraybhog@gmail.com>
2026-05-02 16:01:39 +0530

    Added words

--- code.cpp
+++ code.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main() {
     cout << "Hare Krishna Hare Rama" << endl;
-    cout << "Chant and be Happy" << endl;
+    cout << "Chant and be Happy, Do chant fixed rounds daily!!!" << endl;
     return 0;
 }
