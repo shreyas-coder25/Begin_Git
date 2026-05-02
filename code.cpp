378f28a6ab7b46a212c89516236ae85255a0a81e
shreyas-coder25 <shreyasraybhog@gmail.com>
2026-05-02 16:05:00 +0530

    Changes in line

--- code.cpp
+++ code.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main() {
     cout << "Hare Krishna Hare Rama" << endl;
-    cout << "Chant and be Happy" << endl;
+    cout << "Chant Your Rounds Daily!" << endl;
     return 0;
 }
